641c3c3f2693e73d0cc10b4c9c67522ea8be3fc1
Collins njau <collinsnjau39@gmail.com>
2017-12-22 00:12:43 +0300

    I have added ability to tweet when followed and after some intervals

--- bot.js
+++ bot.js
@@ -8,6 +8,25 @@ console.log(Config)
 
 var T = new Twit(Config)
 
+
+
+//setting up a user stream
+var stream = T.stream('user'); //set up a stream to the twitter api
+
+//anytime someone follows you
+stream.on('follow', followed); //assign a callback called followed to the follow activity
+
+function followed(eventMsg) {
+var name=eventMsg.source.name;
+var screenName=eventMsg.source.screen_name;//get twitter account name
+tweetItFollow('@'+screenName + ' do you like rainbows?')
+}
+
+
+
+
+
+
 var params = {
   q: 'scotch.io', //search tweet with this keyword
   count: 3 //number of tweets to return
@@ -26,13 +45,37 @@ function getData(err, data, response) {
 
   //the callback function
 }
+
+
 tweetIt()
-setInterval(tweetIt,1000*20)
-//POST A tweet
 function tweetIt() {
-  var r=Math.floor(Math.random()*100)
+  //var r = Math.floor(Math.random() * 100)
   var tweet = {
-      status: '#CodingChallenges with exercism '+ r
+    status: "The code train"
+  }
+  T.post('statuses/update', tweet, tweeted);
+
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log("something went wrong")
+    } else {
+      console.log("It worked")
+    }
+  }
+}
+
+
+
+
+
+
+
+//setInterval(tweetIt, 1000 * 20)
+//POST A when followed
+function tweetItFollow(txt) {
+  //var r = Math.floor(Math.random() * 100)
+  var tweet = {
+    status: txt
   }
   T.post('statuses/update', tweet, tweeted);
 
