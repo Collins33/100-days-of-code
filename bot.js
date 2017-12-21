3cb67961e069dc0def4be1ff2b4848c9e229172f
Collins njau <collinsnjau39@gmail.com>
2017-12-21 22:09:28 +0300

    I have added an interval of 20 seconds after which the bot runs

--- bot.js
+++ bot.js
@@ -1,40 +1,46 @@
 console.log("my bot")
+var allTweets = [];
 
-var Twit=require('twit');//get the twit package
-var Config=require('./config');//get the access tokens
+var Twit = require('twit'); //get the twit package
+var Config = require('./config'); //get the access tokens
 
 console.log(Config)
 
 var T = new Twit(Config)
 
-var params={
-  q:'moringa school',//search tweet with this keyword
-  count:3//number of tweets to return
+var params = {
+  q: 'scotch.io', //search tweet with this keyword
+  count: 3 //number of tweets to return
 }
 
 
-T.get('search/tweets',params,getData);//tell twitter to SEARCH for tweets
+T.get('search/tweets', params, getData); //tell twitter to SEARCH for tweets
 //getdata is for handling the response
 
-function getData(err,data,response){
-  var tweets=data.statuses;
-  for (var i=0;i < tweets.length;i ++){
-    console.log(tweets[i].text);
+function getData(err, data, response) {
+  tweets = data.statuses;
+  for (var i = 0; i < tweets.length; i++) {
+    allTweets.push(tweets[i].text)
+    console.log(allTweets);
   }
 
   //the callback function
 }
-
+tweetIt()
+setInterval(tweetIt,1000*20)
 //POST A tweet
-var tweet={
-  status:'#CodingChallenges with twot'
-}
-T.post('statuses/update',tweet,tweeted);
-function tweeted(err,data,response){
-  if(err){
-    console.log("something went wrong")
+function tweetIt() {
+  var r=Math.floor(Math.random()*100)
+  var tweet = {
+      status: '#CodingChallenges with exercism '+ r
   }
-  else{
-    console.log("It worked")
+  T.post('statuses/update', tweet, tweeted);
+
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log("something went wrong")
+    } else {
+      console.log("It worked")
+    }
   }
 }
