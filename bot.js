0d8b868de05e7533d0baf3d3a480d355d91d2216
Collins njau <collinsnjau39@gmail.com>
2017-12-21 20:08:40 +0300

    I have added the get request to the twitter api to receive the tweets with the passed parameter

--- bot.js
+++ bot.js
@@ -6,3 +6,16 @@ var Config=require('./config');//get the access tokens
 console.log(Config)
 
 var T = new Twit(Config)
+
+var params={
+  q:'banana since:2011-11-11',//search tweet with this keyword
+  count:100//number of tweets to return
+}
+
+
+T.get('search/tweets',params,getData);//tell twitter to search for tweets
+
+function getData(err,data,response){
+  console.log(data);
+  //the callback function
+}
