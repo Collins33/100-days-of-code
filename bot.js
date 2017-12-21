196a1adbca3527ef683a894052d851ddb6066684
Collins njau <collinsnjau39@gmail.com>
2017-12-21 19:32:11 +0300

    I have placed the api keys in a separate file

--- bot.js
+++ bot.js
@@ -5,7 +5,4 @@ var Config=require('./config');//get the access tokens
 
 console.log(Config)
 
-// var T = new Twit({
-//
-//   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-// })
+var T = new Twit(Config)
