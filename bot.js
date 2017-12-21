5334392590b832ecc6944bb664794c445bb6255d
Collins njau <collinsnjau39@gmail.com>
2017-12-21 19:30:33 +0300

    I have installed the twit package and received the access tokens from twitter

---
+++ bot.js
@@ -0,0 +1,11 @@
+console.log("my bot")
+
+var Twit=require('twit');//get the twit package
+var Config=require('./config');//get the access tokens
+
+console.log(Config)
+
+// var T = new Twit({
+//
+//   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+// })
