de7cd4f2b8abf252d97890ffb61e5a4aa34fad9c
Collins njau <collinsnjau39@gmail.com>
2017-12-22 16:17:43 +0300

    I have added the js method to retweet a specific tweet

--- bot.js
+++ bot.js
@@ -13,65 +13,16 @@ var T = new Twit(Config)
 //setting up a user stream
 var stream = T.stream('user'); //set up a stream to the twitter api
 
-//anytime someone follows you
+//BOT TO TWEET WHEN SOMEONE FOLLOWS ME
 stream.on('follow', followed); //assign a callback called followed to the follow activity
 
 function followed(eventMsg) {
-var name=eventMsg.source.name;
-var screenName=eventMsg.source.screen_name;//get twitter account name
-tweetItFollow('@'+screenName + ' do you like rainbows?')
+  var name = eventMsg.source.name;
+  var screenName = eventMsg.source.screen_name; //get twitter account name
+  tweetItFollow('@' + screenName + ' thank you for following')
 }
-
-
-
-
-
-
-var params = {
-  q: 'scotch.io', //search tweet with this keyword
-  count: 3 //number of tweets to return
-}
-
-
-T.get('search/tweets', params, getData); //tell twitter to SEARCH for tweets
-//getdata is for handling the response
-
-function getData(err, data, response) {
-  tweets = data.statuses;
-  for (var i = 0; i < tweets.length; i++) {
-    allTweets.push(tweets[i].text)
-    console.log(allTweets);
-  }
-
-  //the callback function
-}
-
-
-tweetIt()
-function tweetIt() {
-  //var r = Math.floor(Math.random() * 100)
-  var tweet = {
-    status: "The code train"
-  }
-  T.post('statuses/update', tweet, tweeted);
-
-  function tweeted(err, data, response) {
-    if (err) {
-      console.log("something went wrong")
-    } else {
-      console.log("It worked")
-    }
-  }
-}
-
-
-
-
-
-
-
 //setInterval(tweetIt, 1000 * 20)
-//POST A when followed
+//POST A TWEET WHEN FOLLOWED
 function tweetItFollow(txt) {
   //var r = Math.floor(Math.random() * 100)
   var tweet = {
@@ -87,3 +38,76 @@ function tweetItFollow(txt) {
     }
   }
 }
+
+
+
+
+
+//GET TWEETS FROM SCOTCH
+var params = {
+  q: 'scotch.io', //search tweet with this keyword
+  count: 3 //number of tweets to return
+}
+T.get('search/tweets', params, getData); //tell twitter to SEARCH for tweets
+//getdata is for handling the response
+function getData(err, data, response) {
+  tweets = data.statuses;
+  for (var i = 0; i < tweets.length; i++) {
+    allTweets.push(tweets[i].text)
+    console.log(allTweets);
+  }
+  //the callback function
+}
+
+//TWEET A SPECIFIC TWEET
+tweetIt()
+
+function tweetIt() {
+  //var r = Math.floor(Math.random() * 100)
+  var tweet = {
+    status: "Invest in good education #devlife #twot"
+  }
+  T.post('statuses/update', tweet, tweeted);
+
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log("something went wrong")
+    } else {
+      console.log("It worked")
+    }
+  }
+}
+
+
+//RETWEET BOT
+var retweet = function() {
+  var params = {
+    q: '#Angularjs, #angularJs',
+    result_type: 'recent',
+    lang: 'en'
+  }
+}
+T.get('search/tweets', params, function(err, data) {
+  //if there are no errors
+  if (!err) {
+    //grab the id of the tweet to RETWEET
+    var retweetId = data.statuses[0].id_str;
+    //tell twitter to retweet
+    T.post('statuses/retweet/:id', {
+      id: retweetId
+    }, function(err, response) {
+      if (response) {
+        console.log('RETWEETED')
+      }
+      if (err) {
+        console.log("SOMETHING WENT WRONG WHEN RETWEETING")
+      }
+    });
+
+  }
+  //if unable to search a tweet
+  else {
+    console.log("SOMETHING WENT WRONG WHEN FINDING TWEET")
+  }
+});
+retweet();
