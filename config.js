c26ab1fac64fa22a219accca26527aa1990e4ea0
Collins njau <collinsnjau39@gmail.com>
2018-01-08 18:14:33 +0300

    Deployment

---
+++ config.js
@@ -0,0 +1,9 @@
+//files that can be used elsewhere
+
+
+module.exports={
+  consumer_key:         'EnBu2hScqwqIfA9rKpPYm8guH',
+  consumer_secret:      'CDF252OZWymP8hUqetkihKutoUpSoJMKbuxm5ui3NNcUH2BQ7u',
+  access_token:         '1897796726-Qlcmn8fftoTY59vyjjGe8zfBIU969jNOoxNMnvs',
+  access_token_secret:  'KaPIjxIJkfUPT7mCOwb9lgMjv3i77lSHrs3AZjSqCoEBF',
+}
